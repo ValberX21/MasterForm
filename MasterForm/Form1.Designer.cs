023c945d5e3d05bbb2175055ce7ea5cf56269b79
ValberX21 <martins_shinigami@hotmail.com>
2022-06-24 08:35:03 -0300

    Add project files.

---
+++ MasterForm/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace MasterForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createTxtandPath = new System.Windows.Forms.Button();
+            this.txtWithSql = new System.Windows.Forms.Button();
+            this.savesql = new System.Windows.Forms.Button();
+            this.txtNomeFant = new System.Windows.Forms.TextBox();
+            this.txtRazaoSocial = new System.Windows.Forms.TextBox();
+            this.txtCNPJ = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // createTxtandPath
+            // 
+            this.createTxtandPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createTxtandPath.Location = new System.Drawing.Point(12, 12);
+            this.createTxtandPath.Name = "createTxtandPath";
+            this.createTxtandPath.Size = new System.Drawing.Size(198, 49);
+            this.createTxtandPath.TabIndex = 0;
+            this.createTxtandPath.Text = "Create txt and path";
+            this.createTxtandPath.UseVisualStyleBackColor = true;
+            this.createTxtandPath.Click += new System.EventHandler(this.createTxtandPath_Click);
+            // 
+            // txtWithSql
+            // 
+            this.txtWithSql.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWithSql.Location = new System.Drawing.Point(216, 12);
+            this.txtWithSql.Name = "txtWithSql";
+            this.txtWithSql.Size = new System.Drawing.Size(198, 49);
+            this.txtWithSql.TabIndex = 1;
+            this.txtWithSql.Text = "Create txt with SQL";
+            this.txtWithSql.UseVisualStyleBackColor = true;
+            this.txtWithSql.Click += new System.EventHandler(this.txtWithSql_Click);
+            // 
+            // savesql
+            // 
+            this.savesql.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.savesql.Location = new System.Drawing.Point(12, 87);
+            this.savesql.Name = "savesql";
+            this.savesql.Size = new System.Drawing.Size(198, 49);
+            this.savesql.TabIndex = 2;
+            this.savesql.Text = "Save in SQL";
+            this.savesql.UseVisualStyleBackColor = true;
+            this.savesql.Click += new System.EventHandler(this.savesql_Click);
+            // 
+            // txtNomeFant
+            // 
+            this.txtNomeFant.AccessibleDescription = "";
+            this.txtNomeFant.Location = new System.Drawing.Point(349, 87);
+            this.txtNomeFant.Name = "txtNomeFant";
+            this.txtNomeFant.Size = new System.Drawing.Size(198, 20);
+            this.txtNomeFant.TabIndex = 3;
+            this.txtNomeFant.Tag = "";
+            // 
+            // txtRazaoSocial
+            // 
+            this.txtRazaoSocial.Location = new System.Drawing.Point(349, 116);
+            this.txtRazaoSocial.Name = "txtRazaoSocial";
+            this.txtRazaoSocial.Size = new System.Drawing.Size(198, 20);
+            this.txtRazaoSocial.TabIndex = 4;
+            // 
+            // txtCNPJ
+            // 
+            this.txtCNPJ.Location = new System.Drawing.Point(349, 142);
+            this.txtCNPJ.Name = "txtCNPJ";
+            this.txtCNPJ.Size = new System.Drawing.Size(198, 20);
+            this.txtCNPJ.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(212, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nome Fantasia";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(228, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 22);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Razão Social";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(286, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 22);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "CNPJ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCNPJ);
+            this.Controls.Add(this.txtRazaoSocial);
+            this.Controls.Add(this.txtNomeFant);
+            this.Controls.Add(this.savesql);
+            this.Controls.Add(this.txtWithSql);
+            this.Controls.Add(this.createTxtandPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createTxtandPath;
+        private System.Windows.Forms.Button txtWithSql;
+        private System.Windows.Forms.Button savesql;
+        private System.Windows.Forms.TextBox txtNomeFant;
+        private System.Windows.Forms.TextBox txtRazaoSocial;
+        private System.Windows.Forms.TextBox txtCNPJ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MasterForm/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasterForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conneciton = new SqlConnection("Data Source=DESKTOP-ULLM2GO\\SQLEXPRESS;Initial Catalog=COMPANIES;User Id=issac;password=Wolverine9");
+
+
+        // _____________________ CLICK TO GENERATE A FILE .txt IN A PATH ________________________
+
+        private void createTxtandPath_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\playground_C#\MasterForm\HereisFileTxt\file.txt";
+            string text = "Lord of the rings" + "\n" + "Hobbit" + "\n" + "Star wards";
+
+            if (!File.Exists(path))
+            {
+                //Create a file and write
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(text);
+                }
+                MessageBox.Show("File created !!!");
+            }
+        }
+
+        // _____________________ CLICK TO GENERATA A txt FILE WITH SQL SERVER ________________
+
+        private void txtWithSql_Click(object sender, EventArgs e)
+        {           
+            SqlCommand command = new SqlCommand("SELECT Id_Empresa, NomeFantasia, CNPJ FROM Empresas", conneciton);
+
+            StringBuilder fileContant = new StringBuilder();
+
+            fileContant.Append("Nome fantasia");
+            fileContant.Append("".PadLeft(15, ' '));
+            fileContant.Append("CNPJ");
+            fileContant.Append("\n");
+
+            try
+            {
+                conneciton.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    fileContant.Append(reader["NomeFantasia"].ToString());
+                    //fileContant.Append(" - ");
+                    fileContant.Append("".PadLeft(15,' '));
+                    fileContant.Append(reader["CNPJ"].ToString());
+                    fileContant.Append("\n");
+                }
+
+                buildTxtFile(fileContant);
+            }
+            catch(SqlException eX)
+            {
+                MessageBox.Show(eX.ToString());
+            }
+            finally
+            {
+                conneciton.Close();
+            }
+        }
+
+        private void buildTxtFile(StringBuilder fileContant)
+        {
+            string timeAJ = DateTime.Now.ToString();
+            string time = timeAJ.Replace(":", "").Replace("/", "").Replace(" ", "");
+
+            string pathFileAJ = @"C:\playground_C#\MasterForm\ListaEmpresas\" + time;
+
+            string pathFile = pathFileAJ.Replace("/", "");
+
+            if (!Directory.Exists(pathFile))
+            {
+                Directory.CreateDirectory(pathFile);
+            }
+
+            if (!File.Exists(pathFile))
+            {
+                string addFormaFile = time + ".txt";
+                string nameFile = addFormaFile.Replace("/", "").Replace(":", "").Replace(" ","");
+
+                // Writing in the file
+
+                using (StreamWriter sw = File.CreateText(pathFile + "\\" + nameFile))
+                {
+                    sw.WriteLine(fileContant);
+                }
+                MessageBox.Show("File created successfully");
+            }
+        }
+
+        //SAVE SOME DATAS IN SQL
+
+        //NomeFantasia, RazoSocial, CNPJ
+        private void savesql_Click(object sender, EventArgs e)
+        {
+            string nomeFantasia = txtNomeFant.Text.ToString();
+            string razaoSocial = txtRazaoSocial.Text.ToString();
+            string CNPJ = txtCNPJ.Text.ToString();
+
+            String queryInsert = "INSERT INTO Empresas (NomeFantasia, RazaoSocial , CNPJ, Email) " +
+                                    " values  (@NomeFantasia, @RazaoSocial, @CNPJ, @Email)";
+            try{
+                conneciton.Open();
+                SqlCommand command = new SqlCommand(queryInsert, conneciton);
+                command.Parameters.Add("@NomeFantasia", nomeFantasia);
+                command.Parameters.Add("@RazaoSocial", razaoSocial);
+                command.Parameters.Add("@CNPJ", CNPJ);
+                command.Parameters.Add("@Email", "emailtest@hotmail.com");
+
+                command.ExecuteNonQuery();
+
+                MessageBox.Show("Data saved with successufully");
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                conneciton.Close();
+            }
+
+
+        }
+    }
+}
