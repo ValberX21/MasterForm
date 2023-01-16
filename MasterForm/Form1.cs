5d5bc90c49f1afb3d32aaeba646901582b13b457
Valber Martins <martins_shinigami@hotmail.com>
2023-01-16 13:12:31 -0300

    Method to use database

--- MasterForm/Form1.cs
+++ MasterForm/Form1.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.IO;
+using System.Text;
 using System.Windows.Forms;
 
 namespace MasterForm
@@ -10,6 +12,9 @@ namespace MasterForm
         {
             InitializeComponent();
         }
+
+        SqlConnection conneciton = new SqlConnection("Data Source=DESKTOP-ULLM2GO\\SQLEXPRESS;Initial Catalog=COMPANIES;User Id=issac;password=Wolverine12");
+
 
         private void createFileText_Click(object sender, EventArgs e)
         {
@@ -34,7 +39,70 @@ namespace MasterForm
 
         private void importMovies_Click(object sender, EventArgs e)
         {
+            SqlCommand command = new SqlCommand("SELECT Id, Titles, Author FROM MANGAS", conneciton);
 
+            StringBuilder fileContant = new StringBuilder();
+
+            fileContant.Append("Id");
+            fileContant.Append("".PadLeft(15, ' '));
+            fileContant.Append("Titles");
+            fileContant.Append("".PadLeft(15, ' '));
+            fileContant.Append("Author");
+            fileContant.Append("\n");
+
+            try
+            {
+                conneciton.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    fileContant.Append(reader["Id"].ToString());
+                    fileContant.Append("".PadLeft(15, ' '));
+                    fileContant.Append(reader["Titles"].ToString());
+                    fileContant.Append("".PadLeft(15, ' '));
+                    fileContant.Append(reader["Author"].ToString());
+                    fileContant.Append("\n");
+                }
+
+                buildTxtFile(fileContant);
+            }
+            catch (SqlException eX)
+            {
+                MessageBox.Show(eX.ToString());
+            }
+            finally
+            {
+                conneciton.Close();
+            }
+        }
+
+        private void buildTxtFile(StringBuilder fileContant)
+        {
+            string timeAJ = DateTime.Now.ToString();
+            string time = timeAJ.Replace(":", "").Replace("/", "").Replace(" ", "");
+
+            string pathFileAJ = @"C:\_developer\playground_C#\MasterForm\New-Files" + time;
+
+            string pathFile = pathFileAJ.Replace("/", "");
+
+            if (!Directory.Exists(pathFile))
+            {
+                Directory.CreateDirectory(pathFile);
+            }
+
+            if (!File.Exists(pathFile))
+            {
+                string addFormaFile = time + ".txt";
+                string nameFile = addFormaFile.Replace("/", "").Replace(":", "").Replace(" ", "");
+
+                // Writing in the file
+
+                using (StreamWriter sw = File.CreateText(pathFile + "\\" + nameFile))
+                {
+                    sw.WriteLine(fileContant);
+                }
+                MessageBox.Show("File created successfully");
+            }
         }
     }
 }
